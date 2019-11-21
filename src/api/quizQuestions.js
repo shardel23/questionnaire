aa7b0a08006f91206beca886162d5ce634369f35
Shahar Dotan <shardel23@gmail.com>
2019-11-21 18:40:48 +0200

    Commited current state

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,93 @@
+var quizQuestions = [
+  {
+    question: "האם עבדת בשנה האחרונה?",
+    answers: [
+      {
+          type: "Microsoft",
+          content: "כן"
+      },
+      {
+          type: "Nintendo",
+          content: "לא"
+      },
+      {
+          type: "Other",
+          content: "אני חושב שכן"
+      },
+      {
+          type: "Sony",
+          content: "זה מורכב..."
+      }
+    ]
+    },
+    {
+      question: "שאלה 2",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "בלה בלה"
+          },
+          {
+              type: "Nintendo",
+              content: "בלו בלו"
+          },
+          {
+              type: "Sony",
+              content: "אחר"
+          }
+      ]
+    },
+    {
+        question: "שאלה 3",
+        answers: [
+            {
+                type: "Microsoft",
+                content: "בלה בלה"
+            },
+            {
+                type: "Nintendo",
+                content: "בלו בלו"
+            },
+            {
+                type: "Sony",
+                content: "אחר"
+            }
+        ]
+    },
+    {
+        question: "שאלה 4",
+        answers: [
+            {
+                type: "Microsoft",
+                content: "בלה בלה"
+            },
+            {
+                type: "Nintendo",
+                content: "בלו בלו"
+            },
+            {
+                type: "Sony",
+                content: "אחר"
+            }
+        ]
+    },
+    {
+        question: "שאלה 5",
+        answers: [
+            {
+                type: "Microsoft",
+                content: "בלה בלה"
+            },
+            {
+                type: "Nintendo",
+                content: "בלו בלו"
+            },
+            {
+                type: "Sony",
+                content: "אחר"
+            }
+        ]
+    }
+];
+
+export default quizQuestions;
---
+++ src/components/Quiz.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {CSSTransitionGroup} from 'react-transition-group';
+import Question from '../components/Question';
+import QuestionCount from '../components/QuestionCount';
+import AnswerOption from '../components/AnswerOption';
+import ProgressBar from "react-bootstrap/ProgressBar";
+
+function Quiz(props) {
+    function renderAnswerOptions(key) {
+        return (
+            <AnswerOption
+                key={key.content}
+                answerContent={key.content}
+                answerType={key.type}
+                answer={props.answer}
+                questionId={props.questionId}
+                onAnswerSelected={props.onAnswerSelected}
+            />
+        );
+    }
+
+    function goBack() {
+        alert('This should go back...')
+    }
+
+    return (
+        <CSSTransitionGroup
+            className="container"
+            component="div"
+            transitionName="fade"
+            transitionEnterTimeout={800}
+            transitionLeaveTimeout={500}
+            transitionAppear
+            transitionAppearTimeout={500}
+        >
+            <div key={props.questionId}>
+                <div className="bootstrap-iso">
+                    <ProgressBar className="right" now={(props.questionId - 1) / props.questionTotal * 100}/>
+                </div>
+                {/*<QuestionCount counter={props.questionId} total={props.questionTotal}/>*/}
+                <Question content={props.question}/>
+                <ul className="answerOptions">
+                    {props.answerOptions.map(renderAnswerOptions)}
+                </ul>
+                {/*<button onClick={goBack}> button</button>*/}
+            </div>
+        </CSSTransitionGroup>
+    );
+}
+
+Quiz.propTypes = {
+    answer: PropTypes.string.isRequired,
+    answerOptions: PropTypes.array.isRequired,
+    question: PropTypes.string.isRequired,
+    questionId: PropTypes.number.isRequired,
+    questionTotal: PropTypes.number.isRequired,
+    onAnswerSelected: PropTypes.func.isRequired
+};
+
+export default Quiz;
